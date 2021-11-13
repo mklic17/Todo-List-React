3e70b389b6384e5440ffb6f4d15a1a4d2f7b89c7
Mitchell Klich <MitchellKlich_2017@DePauw.edu>
2021-11-13 15:40:07 -0500

    Added base todo Page

--- src/App.js
+++ src/App.js
@@ -1,38 +1,41 @@
-import React, { useReducer } from 'react'
-import Navbar from './fragments/navbar'
-import { mount, route } from 'navi';
-import { Router, View } from 'react-navi';
-import appReducer from './Reducer'
-import 'bootstrap/dist/css/bootstrap.css'
-import { StateContext } from './Context'
-import Homepage from './pages/Homepage';
+	import React, { useReducer } from 'react'
+	import Navbar from './fragments/navbar'
+	import { mount, route } from 'navi';
+	import { Router, View } from 'react-navi';
+	import appReducer from './Reducer'
+	import 'bootstrap/dist/css/bootstrap.css'
+	import { StateContext } from './Context'
+	import Homepage from './pages/Homepage';
+	import Todopage from './pages/Todopage';
 
- 
-function App() {
 
-  const [state, dispatch] = useReducer(appReducer, {user: '', toDo: []});
-  const { user } = state;
-  
-  const routes = mount({
-    '/': route({view: <Homepage/>}),
-    // future
-    // '/todo/:id': route({view: <ToDoEntry>}),
-    // '/user/:id': route({view <UserPage/>})
-    // '/user/all': route({view <AllUsers/>})
-  })
+	function App() {
 
-  return (
-    <div>
-      <StateContext.Provider value={{state: state, dispatch: dispatch}}>
-        <Router routes = {routes}>
-          <Navbar/>
-          <div className="container">
-            <View/>
-          </div>
-        </Router>
-      </StateContext.Provider>
-    </div>
-  )
-}
+	const [state, dispatch] = useReducer(appReducer, {user: '', toDo: []});
+	const { user } = state;
 
-export default App;
+	const routes = mount({
+	'/': route({ view: <Homepage/> }),
+	'/todo/:id': route(req => {
+		return { view: <Todopage id={req.params.id} /> }
+	}),
+	// future
+	// '/user/:id': route({view <UserPage/>}),
+	// '/user/all': route({view <AllUsers/>})
+	})
+
+	return (
+	<div>
+		<StateContext.Provider value={{state: state, dispatch: dispatch}}>
+		<Router routes = {routes}>
+			<Navbar/>
+			<div className="container">
+				<View/>
+			</div>
+		</Router>
+		</StateContext.Provider>
+	</div>
+	)
+	}
+
+	export default App;
