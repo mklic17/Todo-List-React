2e5d0b5e2f10c31c6c88000d0559a256ad3d5532
Mitchell Klich <MitchellKlich_2017@DePauw.edu>
2021-11-13 09:10:06 -0500

    added the HomePage and began refactoring into routes

--- src/App.js
+++ src/App.js
@@ -1,63 +1,32 @@
-import React, { useReducer, useEffect } from 'react'
+import React, { useReducer } from 'react'
 import Navbar from './fragments/navbar'
-import ToDoList from './todo/ToDoList'
-import CreateToDoEntry from './todo/CreateToDoEntry'
+import { mount, route } from 'navi';
+import { Router, View } from 'react-navi';
 import appReducer from './Reducer'
-import Login from './auth/Login'
-import Registration from './auth/Registration'
 import 'bootstrap/dist/css/bootstrap.css'
 import { StateContext } from './Context'
-import { useResource } from 'react-request-hook';
+import Homepage from './pages/Homepage';
+
  
 function App() {
 
-  const [toDos, getToDos] = useResource(() => ({
-    url: '/todos',
-    method: 'get'
-  }));
-
   const [state, dispatch] = useReducer(appReducer, {user: '', toDo: []});
   const { user } = state;
+  
+  const routes = mount({
+    '/': route({view: <Homepage/>}),
 
-  useEffect(getToDos, []); // initial get of all the Posts
-
-
-  useEffect(() => {
-      if (toDos && toDos.data) {
-          dispatch({ type: 'FETCH_POSTS', toDos: toDos.data })
-      }
-  }, [toDos])
-
-  let main;
-  if(user)
-    main = 
-      <div className="row">
-          <div className="col-md-6">
-            <CreateToDoEntry />
-          </div>
-          <div className="col-md-6">
-            <ToDoList/>
-          </div> 
-      </div>
-    else {
-      main = 
-        <div className="row">
-          <div className="col-md-6">
-            <Login/>
-          </div>
-          <div className="col-md-6">
-            <Registration/>
-          </div>
-        </div>
-    }
+  })
 
   return (
     <div>
       <StateContext.Provider value={{state: state, dispatch: dispatch}}>
-        <Navbar/>
-        <div className="container">
-          { main }
-        </div>
+        <Router routes = {routes}>
+          <Navbar/>
+          <div className="container">
+            <View/>
+          </div>
+        </Router>
       </StateContext.Provider>
     </div>
   )
